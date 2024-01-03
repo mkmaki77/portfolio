d42b7a7751b2e6e804b0dedc9e95aac6b31fe24e
mkmaki77 <you92075@gmail.com>
2024-01-03 13:58:16 +0800

    V3

--- js/all.js
+++ js/all.js
@@ -1,5 +1,12 @@
 $(document).ready (function(){
+// index //
 
 
+ // about //   
+ $('.s1').hide().fadeIn(1000);
+ $('.s2').hide().delay(500).fadeIn(1000);
+
+// contact //
+$('.content').hide().fadeIn(1000);
 
 })
